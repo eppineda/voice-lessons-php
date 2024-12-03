80c2503950fbfd9d31ddce22e945f418ddc41159
bit mechanix llc <enrique@bit-mechanix.com>
2024-12-02 23:16:02 -0500

    parse the xml object representation of the catalog and render as html

--- app/controllers/all.php
+++ app/controllers/all.php
@@ -3,63 +3,41 @@
 use Monolog\Level;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
-require '../app/models/catalog.php';
 
 $logger = new Logger(__FILE__);
 $logger->pushHandler(new StreamHandler('php://stdout', Level::Debug));
-$logger->pushHandler(new StreamHandler('/error.log', Level::Error));
-$logger->pushHandler(new StreamHandler('/critical.log', Level::Critical));
-
-function Xml2Json($fileContents) {
-	global $logger;
-
-	$fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
-        $fileContents = trim(str_replace('"', "'", $fileContents));
-	$simpleXml = simplexml_load_string($fileContents); //logger->debug($simpleXml);
-	$json = json_encode($simpleXml); //logger->debug($json);
-	return $json;
-}
+$logger->pushHandler(new StreamHandler('php://stdout', Level::Error));
+$logger->pushHandler(new StreamHandler('php://stdout', Level::Critical));
 
 function all() {
 	global $logger;
+	$content = '';
 
 	define('REQUEST', 'data/raw.xml');
-
-	$xml = file_get_contents(REQUEST); $logger->debug($xml);
-	$json = Xml2Json($xml); $logger->debug($json);
-	define('DECODED', json_decode($json, true));
-	define('COURSES', courses(DECODED));
-	define('FIRST', course(DECODED, 0));
-
-	$content = '';
-// header
-	$logger->debug('header start');	
-	$fieldNames = [];
-	$content .= '<tr bgcolor="#9acd32">';
-	foreach (FIRST.elements as $f) {
-		define('FIELDNAME', $f.name);
-
-		$fieldNames[] = FIELDNAME;
-		$content .= "<th style=\"text-align: left\">\${ FIELDNAME }</th>";
-	}
-	$content .= '</tr>';
-	$logger->debug('header end');	
-
+	define('S', file_get_contents(REQUEST)); $logger->debug(S);
+	define('CATALOG', new SimpleXMLElement(S));
 // data
-	$logger->debug('data start');	
-	$idx = 0;
-	foreach (COURSES as $c) {
-		$content .= '<tr>';
-		$idx2 = 0;
-		foreach ($fieldNames as $f) {
-			define('FIELD_DATA', field($json, $idx++, $idx2++));
+	$logger->debug('data start');
 
-			$content .= "<td>\${ FIELD_DATA }</td>";
-		}
-		$content .= '</tr>';
+	foreach (CATALOG->course as $c) {
+		$content .= '<tr>';
+
+// id
+		$id = $c->id;
+		$content .= '<td>'.$id.'</td>';
+
+// title
+		$title = $c->title;
+		$content .= '<td>'.$title.'</td>';
+
+// description
+		$description = $c->description;
+		$content .= '<td>'.$description.'</td>';
+
+		$content .= '</tr>'; $logger->debug($content);
 	}
+
 	$logger->debug('data end');	
-	
 	$logger->debug($content);
 	return $content;
 }
